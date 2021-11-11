2a9aa2315e3aabb270d953aafd9c8c7055ed169a
YigalB1 <yigalb@hotmail.com>
2021-11-11 12:08:29 +0200

    working with potentiometer

---  linear_actuator_wemos/src/main.cpp
+++  linear_actuator_wemos/src/main.cpp
@@ -1,70 +1,119 @@
-#include <Arduino.h>
+//#include <Arduino.h>
 #include <servo.h>
 
-#define SERVO_OPEN   0
-#define SERVO_CLOSE  180 
-#define SERVO_INC    10
-#define SERVO_DELAY  200
+uint8_t const SERVO_INC   = 10;
+uint8_t const SERVO_MIN_angle   = 0;
+uint8_t const SERVO_MAX_angle   = 180;
+uint8_t const SERVO_DELAY = 200;
+int const SERVO_MIN_us   = 600;
+int const SERVO_MAX_us   = 2800;
 
-uint8_t Servo1_pin = 0;
-uint8_t Servo2_pin = 4;
 
-bool led_toggle = false;
+uint8_t const SERVO1_PIN   = 4; // was0;
+
+const int analogInPin = A0;  // ESP8266 Analog Pin ADC0 = A0
+int sensorValue = 0;  // value read from the pot
+
+bool run_endless = false;
 Servo MyServo1;
-Servo MyServo2;
 
-int servo_jump = int((SERVO_CLOSE-SERVO_OPEN)/SERVO_INC);
+int servo_jump;
+int servo_angle;
 
-void move_servo(Servo _servo,int _start,int _end,int _inc);
+void my_wait(int ms_period);
 
 void setup(){
-  pinMode(LED_BUILTIN, OUTPUT);
-  MyServo1.attach(Servo1_pin);
-    
   Serial.begin(9600);
   Serial.println("starting  setup()");
   Serial.println(ESP.getChipId());
 
+  pinMode(LED_BUILTIN, OUTPUT);
+  MyServo1.attach(SERVO1_PIN,SERVO_MIN_us,SERVO_MAX_us);
+
+  Serial.print("  ON  ");
   digitalWrite(LED_BUILTIN, LOW);
-  MyServo1.write(SERVO_CLOSE);
-  delay(300);
-  MyServo1.write(SERVO_OPEN);
-  delay(300);
-   MyServo1.write(SERVO_CLOSE);
-  delay(300);
+  MyServo1.write(SERVO_MAX_angle);
+  delay(1000);
+  //yig_wait(1000);
+
+  Serial.print("  OFF  ");
   digitalWrite(LED_BUILTIN, HIGH);
+  MyServo1.write(SERVO_MIN_angle);
+  delay(1000);
+  //yig_wait(1000);   
+
+  servo_jump = int((SERVO_MAX_angle-SERVO_MIN_angle)/SERVO_INC);
+  Serial.print("servo_jump: ");
+  Serial.println(servo_jump);
+
 } // of setup
 
-void loop(){
 
-  move_servo(MyServo1,SERVO_OPEN,SERVO_CLOSE,SERVO_INC);
-  delay(1000);
-  move_servo(MyServo1,SERVO_CLOSE,SERVO_OPEN,SERVO_INC);
-  delay(1000);
+int tmp_1;
+int tmp_2;
+bool led_tmp = false;
+
+void loop(){
+  
+    
+    
+    /*
+    for (int i=800;i<=2500;i+=100) {
+      MyServo1.writeMicroseconds(i);
+      Serial.print("i: ");
+      Serial.print(i);
+      tmp_2 = MyServo1.readMicroseconds();
+      Serial.print("   uS: ");
+      Serial.println(tmp_2);
+      delay(400);
+    }
+    delay(1000);
+    if (led_tmp) {
+      digitalWrite(LED_BUILTIN, LOW); // turn led on
+      led_tmp = false;
+    } // of if()
+    else {
+      digitalWrite(LED_BUILTIN, HIGH); // turn led off
+      led_tmp = true;
+    }
+    
+    return;
+    */
+
+
+    sensorValue = analogRead(analogInPin);
+    // tmp_1 = map(sensorValue, 0, 1023, 1000, 2000);
+    
+    
+    //Serial.print(" ");
+    Serial.print(sensorValue);
+    Serial.print(" ");
+
+    // translate the potentiometer value to the microseconds span
+    // of the servo (better than angle)
+    servo_angle = map(sensorValue, 0, 1023, SERVO_MIN_us, SERVO_MAX_us);
+    Serial.print(servo_angle);
+
+    MyServo1.write(servo_angle);
+    tmp_1 = MyServo1.read();
+    tmp_2 = MyServo1.readMicroseconds();
+    Serial.print(" ");
+    Serial.print(tmp_1);
+    Serial.print(" ");
+    Serial.println(tmp_2);
+
+
+    delay(200);
+    //yig_wait(5000);
+  
 
 } // of loop()
 
-void move_servo(Servo _servo,int _start,int _end,int _inc) {
-  
-  if (_end>_start) {
-    Serial.print("going UP ");
-    for (int i=_start;i<_end;i+=_inc) {
-      Serial.print(i);
-      Serial.print("  ");
-      _servo.write(i);
-      delay(SERVO_DELAY);
-    } // of for loop
-  } // of if()
-  else {
-    Serial.print("going DOWN ");
-      for (int i=_end;i>_start;i-=_inc) {
-        Serial.print(i);
-        Serial.print("  ");
-        _servo.write(i);
-      delay(SERVO_DELAY);
-    } // of for loop
-  } // of else()
-  Serial.println("     next ");
-  
 
-} // of move_servo
+
+void my_wait(int ms_period) {
+  unsigned long time_now = millis();
+  while(millis() < time_now + ms_period){        
+  } // of while()
+} // of my_wait()
+
